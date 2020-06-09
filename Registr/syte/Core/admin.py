de5c0ab74db4b7c99a2df55d2b717925fe1f2b87
Rampat13 <64975372+Rampat13@users.noreply.github.com>
2020-06-09 15:55:22 +0600

    Add files via upload

---
+++ Registr/syte/Core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Core.models import Articles
+
+# Register your models here.
+
+admin.site.register(Articles)
---
+++ Registr/syte/Core/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Articles
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Articles
+        fields = ('name','text')
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args,**kwargs)
+        for field in self.fields:
+                self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class AuthUserForm(AuthenticationForm,forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','password')
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args,**kwargs)
+        for field in self.fields:
+                self.fields[field].widget.attrs['class'] = 'form-control'
+
+class RegisterUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','password')
+    def __init__(self, *args,**kwargs):
+            super().__init__(*args,**kwargs)
+            for field in self.fields:
+                    self.fields[field].widget.attrs['class'] = 'form-control'
+    def save(self, commit = True):
+        user = super().save(commit = False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
